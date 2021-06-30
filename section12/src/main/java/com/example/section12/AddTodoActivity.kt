df2db59207932d016dd5fb0b81481bb0c5209770
lkc <myprose78@gmail.com>
2021-06-30 11:02:48 +0900

    12. Todo앱 Event

--- section12/src/main/java/com/example/section12/AddTodoActivity.kt
+++ section12/src/main/java/com/example/section12/AddTodoActivity.kt
@@ -11,6 +11,11 @@ import kotlinx.android.synthetic.main.activity_add_todo.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+// SAM(Single Abstract Method) 기법 적용
+/*
+    btn.setOnClickListener {
+    }
+ */
 class AddTodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +40,19 @@ class AddTodoActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //액티비티에 메뉴를 적용하려면 자바코드에서 MenuInflater를 이용
         menuInflater.inflate(R.menu.menu_add, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    // onCreateOptionsMenu(Menu menu) 메뉴가 만들어 질때 최초 한번 호출
+    // onPrepareOptionsMenu(Menu menu) 메뉴가 화면에 보일 때마다 반복 호출
+    // 메뉴구성을 위한 함수
+    // add(CharSequence title)
+    // add(int groupId, int itemId, int order, int titleRes)
+    // add(int groupId, int itemId, int order, CharSequence title)
+    // setIcon(int iconRes)
+    // setTitle, setVisible, setEnable
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == R.id.menu_add) {
             if(addTitleEditView.text.toString() != null && addContentEditView.text.toString() != null) {
@@ -54,7 +68,7 @@ class AddTodoActivity : AppCompatActivity() {
                 db.insert("tb_todo", null, contentValues)
                 db.close()
 
-                setResult(RESULT_OK, )
+                setResult(RESULT_OK)
                 finish()
             }
         }
--- section12/src/main/java/com/example/section12/TodoList.kt
+++ section12/src/main/java/com/example/section12/TodoList.kt
@@ -1,9 +1,20 @@
 package com.example.section12
 
 import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_header.view.*
+import kotlinx.android.synthetic.main.item_main.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 // backgroundTint - 그냥 색상
 // ripple - ripple effect 눌렀을때 살짝 변했다가 다시 바뀌는 색상
@@ -14,6 +25,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 // 결과 되돌리기 : 사용자가 뒤로가기 버튼을 누르지 않고도 코드에서 자동으로 화면이 되돌아오게 처리
 // startActivityForResult() 함수를 이용
 class TodoList : AppCompatActivity() {
+
+    var list : MutableList<ItemVO> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,5 +36,127 @@ class TodoList : AppCompatActivity() {
             //startActivity(intent)
             startActivityForResult(intent, 10)
         }
+
+        selectDB()
+    }
+
+    fun selectDB() {
+        list = mutableListOf()
+        val helper = DBHelper(this)
+        val db = helper.writableDatabase
+        val cursor = db.rawQuery("select * from tb_todo order by date desc", null)
+        var predate:Calendar?=null
+        while(cursor.moveToNext()) {
+            val dbDate = cursor.getString(3)
+            val date = SimpleDateFormat("yyyy-MM-dd").parse(dbDate)
+            val currentDate = GregorianCalendar()
+            currentDate.time = date
+            if(!currentDate.equals(predate)) {
+                val headerItem = HeaderItem(dbDate)
+                list.add(headerItem)
+                predate = currentDate
+            }
+            val completed = cursor.getInt(4) != 0
+            val dataItem = DataItem(
+                cursor.getInt(0),
+                cursor.getString(1),
+                cursor.getString(2),
+                completed
+            )
+            list.add(dataItem)
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(list)
+        recyclerView.addItemDecoration(MyDecoration())
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 10 && resultCode == RESULT_OK) {
+            selectDB()
+        }
+    }
+
+    class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val headerView = view.itemHeaderView
+    }
+    class DataViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val itemTitleView = view.itemTitleView
+        val itemContentView = view.itemContentView
+    }
+
+    inner class MyAdapter(val list: MutableList<ItemVO>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            if(viewType == ItemVO.TYPE_HEADER) {
+                val inflater = LayoutInflater.from(parent.context)
+                return HeaderViewHolder(inflater.inflate(R.layout.item_header, parent, false))
+            } else {
+                val inflater = LayoutInflater.from(parent.context)
+                return DataViewHolder(inflater.inflate(R.layout.item_main, parent, false))
+            }
+        }
+
+        // 항목을 구성하기 위해
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val itemVO = list.get(position)
+            if(itemVO.type == ItemVO.TYPE_HEADER) {
+                val viewHolder = holder as HeaderViewHolder
+                val headerItem = itemVO as HeaderItem
+                viewHolder.headerView.text = headerItem.date
+            } else {
+                val viewHolder = holder as DataViewHolder
+                val dataItem = itemVO as DataItem
+                viewHolder.itemTitleView.text = dataItem.title
+                viewHolder.itemContentView.text = dataItem.content
+            }
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return list.get(position).type
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+    }
+
+    inner class MyDecoration:RecyclerView.ItemDecoration() {
+        //각각의 항목을 꾸밈
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+            val index = parent!!.getChildAdapterPosition(view)
+            val itemVO = list.get(index)
+            if (itemVO.type == ItemVO.TYPE_DATA) {
+                view!!.setBackgroundColor(0xFFFFFFFF.toInt())
+                ViewCompat.setElevation(view, 10.0f)
+            }
+
+            outRect!!.set(20, 10, 20, 10)
+        }
     }
 }
+
+abstract class ItemVO {
+    abstract val type : Int
+    companion object {
+        val TYPE_HEADER = 0
+        val TYPE_DATA = 1
+    }
+}
+
+class HeaderItem(var date:String): ItemVO() {
+    override val type: Int
+        get() = ItemVO.TYPE_HEADER
+}
+
+class DataItem(var id: Int, var title:String, var content:String, var completed:Boolean = false)
+    : ItemVO() {
+    override val type: Int
+        get() = ItemVO.TYPE_DATA
+    }
